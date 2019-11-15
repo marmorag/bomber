087465cc7e8434bc5560306fe5089e5a590d05d4
marmorag <g.marmo@hotmail.fr>
2019-11-15 15:11:18 +0100

    error handling & body

--- pkg/worker.go
+++ pkg/worker.go
@@ -2,7 +2,9 @@ package pkg
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -11,7 +13,20 @@ func Worker(id int, jobs <-chan Job, results chan<- Job) {
 		fmt.Println(fmt.Sprintf("worker %d : started job %d", id, job.Id))
 
 		start := time.Now()
-		response, _ := http.Get(job.Url)
+		response, err := http.Get(job.Url)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		_, _ = ioutil.ReadAll(response.Body)
+		err = response.Body.Close()
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
 
 		job.Time = time.Since(start).Seconds()
 		job.Response = response.StatusCode
